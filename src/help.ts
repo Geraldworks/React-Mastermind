267327880dcfd5f85feef058469535603b0a08aa
Gerald Ho <97504394+Geraldworks@users.noreply.github.com>
2024-07-05 19:40:46 +0800

    setting up config files

---
+++ src/help.ts
@@ -0,0 +1,3 @@
+const fn = (a: number, b: number) => a + b;
+
+console.log(fn(1, 2));
---
+++ src/test.tsx
@@ -0,0 +1,9 @@
+const fn = (a: number, b: number) => {
+  console.log(a + b);
+};
+
+fn(1, 2);
+
+const obj = [1];
+
+console.log(obj);
